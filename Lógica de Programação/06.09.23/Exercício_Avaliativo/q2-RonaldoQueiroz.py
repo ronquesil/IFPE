76e4a4a6ac6be5a4f32a74964f4284edc194c72e
Ronaldo Queiroz <ronquesil@gmail.com>
2023-09-12 21:23:26 -0300

    Create q2-RonaldoQueiroz.py

---
+++ Lógica de Programação/06.09.23/Exercício_Avaliativo/q2-RonaldoQueiroz.py
@@ -0,0 +1,8 @@
+# Solicitando um valor em metros do usuário
+metros = float(input("Por favor, insira um valor em metros: "))
+
+# Convertendo o valor de metros para milímetros (1 metro = 1000 milímetros)
+milimetros = metros * 1000
+
+# Imprimindo o resultado da conversão
+print(f"O valor em milímetros é: {milimetros} mm")
