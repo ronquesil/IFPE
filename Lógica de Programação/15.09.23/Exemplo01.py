d3a66d439836b2fc710d969be8b1278f126313a0
Ronaldo Queiroz <ronquesil@gmail.com>
2023-09-15 20:46:36 -0300

    Create Exemplo01.py
    Exemplo 1 – lê dois valores e imprime
    qual o maior

---
+++ Lógica de Programação/15.09.23/Exemplo01.py
@@ -0,0 +1,7 @@
+#Faça um programa que lê dois valores e imprime qual o maior valor
+a = int(input())
+b = int(input())
+if a > b:
+    print("Primeiro valor maior")
+if b > a:
+    print("Segundo valor maior")
