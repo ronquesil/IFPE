8f6c9c4cf874954682dec96aaac313418426f61e
Ronaldo Queiroz <ronquesil@gmail.com>
2023-09-19 21:34:46 -0300

    Update q4-RonaldoQueiroz.py

--- Lógica de Programação/15.09.23/ExercicioAvaliativo02/q4-RonaldoQueiroz.py
+++ Lógica de Programação/15.09.23/ExercicioAvaliativo02/q4-RonaldoQueiroz.py
@@ -2,5 +2,5 @@ x1 = float(input())
 y1 = float(input())
 x2 = float(input())
 y2 = float(input())
-distancia = (((x2-x1)**2)+((y2-y1)**2))**0,5
-print(f"{distancia:.4f}")
+distancia = round(((((x2-x1)**2)+((y2-y1)**2))**0.5) , 4)
+print(distancia)
