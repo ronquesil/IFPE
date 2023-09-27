45264769c31b60b97ff4d049ab9e43ccc907daa0
Ronaldo Queiroz <ronquesil@gmail.com>
2023-09-27 20:49:34 -0300

    Create exemplo02_repeticao_for.py

---
+++ Lógica de Programação/27.09.23/exemplo02_repeticao_for.py
@@ -0,0 +1,13 @@
+num = int(input())
+
+for i in range(num):
+    if i % 2 == 0:
+        print(i)
+
+print(10*"*")
+
+num1 = int(input())
+num2 = int(input())
+for i in range(num1, num2+1):
+    if i % 2 == 0:
+        print(i)
