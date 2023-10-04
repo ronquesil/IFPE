7e57de460348709be514dba56ac2846a99ae4a4d
Ronaldo Queiroz <ronquesil@gmail.com>
2023-10-04 20:08:50 -0300

    Create q01-RonaldoQueiroz.py

---
+++ Lógica de Programação/04.10.23/Exercicio_Aula_05/q01-RonaldoQueiroz.py
@@ -0,0 +1,21 @@
+def calcula_credito(saldo_medio):
+    if saldo_medio > 400:
+        percentual_credito = 0.30
+    elif 300 <= saldo_medio <= 400:
+        percentual_credito = 0.25
+    elif 200 <= saldo_medio < 300:
+        percentual_credito = 0.20
+    else:
+        percentual_credito = 0.10
+
+    valor_credito = saldo_medio * percentual_credito
+    return valor_credito
+
+# Entrada de dados
+saldo_medio = float(input("Digite o saldo médio do cliente: ").replace(',', '.'))
+
+# Chamada da função
+valor_credito = calcula_credito(saldo_medio)
+
+# Saída de dados
+print(f"O valor do crédito concedido ao cliente é: R$ {valor_credito:.2f}")
