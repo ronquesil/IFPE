48f1950edd00af96272a887f979844576a874d60
Ronaldo Queiroz <ronquesil@gmail.com>
2023-12-20 20:59:31 -0300

    Update q2-RonaldoQueiroz.py

--- Lógica de Programação/Avaliacao2unidade/q2-RonaldoQueiroz.py
+++ Lógica de Programação/Avaliacao2unidade/q2-RonaldoQueiroz.py
@@ -1,9 +1,6 @@
-# Recebe uma string de números representando os resultados de 20 lançamentos de um dado
-resultados = input("Insira os resultados dos lançamentos do dado: ")
+resultados = input()
 
-# Conta a frequência de cada número
 frequencias = [resultados.count(str(i)) for i in range(1, 7)]
 
-# Imprime a frequência de cada número
 for i, freq in enumerate(frequencias):
     print(f"Número {i+1} saiu {freq} vezes.")
